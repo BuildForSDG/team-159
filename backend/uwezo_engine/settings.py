e56b59a943b9e6cbd4e2da11994d3a1bd965a8a5
patrick <nderitupatrick4@gmail.com>
2020-05-06 11:09:01 +0300

    added Readme with guide on how to setup the backend project added accounts app for user managment added requirements directory for dependency management added development items to git ignore

--- backend/uwezo_engine/settings.py
+++ backend/uwezo_engine/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    # uwezo apps
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
