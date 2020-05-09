749d7c6c6cbc8b627fe69af72f95ed6e07adce7b
patrick <nderitupatrick4@gmail.com>
2020-05-09 16:13:52 +0300

    added test case for user registration

--- backend/accounts/tests/test_views.py
+++ backend/accounts/tests/test_views.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from django.core import mail
 from accounts.tests.factories.model_factory import UserFactory
 from accounts.models import User
 
@@ -23,3 +24,34 @@ class TestLogin:
         assert result.status_code == 401
         assert result.data['detail'] == 'No active account found with the given credentials'
 
+class TestUserRegistration:
+
+    def test_registration(self, api_client):
+        url  = reverse('accounts:customers')
+        username='customer1'
+        email='customer1@biz.com'
+        first_name = 'customer1'
+        last_name = 'loan'
+        password='customer1'
+        payload =  dict(
+            username=username,
+            email=email,
+            first_name = first_name,
+            last_name = last_name,
+            password=password
+        )
+        api_client.post(url, payload)
+        
+        # ensure the user is in database
+        user =  User.objects.get(
+            username=username,
+            email=email,
+            first_name = first_name,
+            last_name = last_name,)
+        
+        assert user
+        # ansure user can login
+        assert password == user.check_password(password)
+
+        # test welcome email is sent
+        assert len(mail.outbox) == 1
