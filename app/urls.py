35d201b128bfc557cdcabd1196739d68410cd77c
Joseph Gitau <50513886+Hibryd@users.noreply.github.com>
2020-05-28 21:51:53 +0300

    investor and user url endpoints

--- app/urls.py
+++ app/urls.py
@@ -5,9 +5,9 @@ from rest_framework.authtoken.views import obtain_auth_token
 app_name = "app"
 
 urlpatterns = [
-    path("users", CreateLoanView.as_view(), name="add-user"),
-    path("user/update/<str:first_name>", UpdateLoanView.as_view(), name="update-user"),
-    path("user/delete/<str:first_name>", DeleteLoanView.as_view(), name="delete-user"),
+    path("users", CreateUserView.as_view(), name="add-user"),
+    path("user/update/<str:first_name>", UpdateUserView.as_view(), name="update-user"),
+    path("user/delete/<str:first_name>", DeleteUserView.as_view(), name="delete-user"),
     path("business", CreateBusinessView.as_view(), name="add-business"),
     path("business/update/<str:name>", UpdateBusinessView.as_view(), name="update-business"),
     path("business/delete/<str:name>", DeleteBusinessView.as_view(), name="delete-business"),
@@ -17,9 +17,9 @@ urlpatterns = [
     path("loan", CreateLoanView.as_view(), name="add-loan"),
     path("loan/update/<str:customer>", UpdateLoanView.as_view(), name="update-loan"),
     path("loan/delete/<str:customer>", DeleteLoanView.as_view(), name="delete-loan"),
-    path("investor", CreateLoanView.as_view(), name="add-investor"),
-    path("investor/update/<str:first_name>", UpdateLoanView.as_view(), name="update-investor"),
-    path("investor/delete/<str:first_name>", DeleteLoanView.as_view(), name="delete-investor"),
+    path("investor", CreateInvestorView.as_view(), name="add-investor"),
+    path("investor/update/<str:first_name>", UpdateInvestorView.as_view(), name="update-investor"),
+    path("investor/delete/<str:first_name>", DeleteInvestorView.as_view(), name="delete-investor"),
     path("api/token", obtain_auth_token, name="obtain-token"),
     path("rest_auth/", include("rest_auth.urls"))
 ]
