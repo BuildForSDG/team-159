e43fd75d28a59a1c2af87205623dcc4bf8abb0d6
Joseph Gitau <50513886+Hibryd@users.noreply.github.com>
2020-05-28 21:40:25 +0300

    User and Investor urls tests

--- app/tests/test_urls.py
+++ app/tests/test_urls.py
@@ -21,7 +21,71 @@ def url_vars(self, path, view_name, route, namespace="app", kwargs=None):
     self.assertEquals(resolved.route, route)
     self.assertEqual(resolved.view_name, view_name)
     self.assertIn("api/v1/", route)
+class InvestorUrlTestsCase(TestCase):
+    """
+    Tests for all urls related to User Model/Api
+    """
+    def setUp(self) -> None:
+        self.route_add = "app:add-user"
+        self.route_update = "app:update-user"
+        self.route_delete = "app:delete-user"
+        self.kwargs = {"first_name": "name"}
 
+    def test_user_read_url(self):
+        """
+        Correct url for reading and posting of a User data
+        :return: None
+        """
+        url_vars(self, path=self.route_add, view_name=self.route_add, route="api/v1/users")
+
+    def test_user_update_url(self):
+        """
+        Correct url for Updating a User Profile
+        :return: None
+        """
+        url_vars(self, path=self.route_update, view_name=self.route_update, kwargs=self.kwargs,
+                 route="api/v1/user/update/<str:first_name>")
+
+    def test_business_delete_url(self):
+        """
+        Correct urls for deleting a User Profile
+        :return: None
+        """
+        url_vars(self, path=self.route_delete, view_name=self.route_delete, kwargs=self.kwargs,
+                 route="api/v1/user/delete/<str:first_name>")
+        
+class InvestorUrlTestsCase(TestCase):
+    """
+    Tests for all urls related to User Model/Api
+    """
+    def setUp(self) -> None:
+        self.route_add = "app:add-investor"
+        self.route_update = "app:update-investor"
+        self.route_delete = "app:delete-investor"
+        self.kwargs = {"first_name": "name"}
+
+    def test_investor_read_url(self):
+        """
+        Correct url for reading and posting of a Investor data
+        :return: None
+        """
+        url_vars(self, path=self.route_add, view_name=self.route_add, route="api/v1/investor")
+
+    def test_business_update_url(self):
+        """
+        Correct url for Updating a Investor Profile
+        :return: None
+        """
+        url_vars(self, path=self.route_update, view_name=self.route_update, kwargs=self.kwargs,
+                 route="api/v1/investor/update/<str:first_name>")
+
+    def test_business_delete_url(self):
+        """
+        Correct urls for deleting a Investor Profile
+        :return: None
+        """
+        url_vars(self, path=self.route_delete, view_name=self.route_delete, kwargs=self.kwargs,
+                 route="api/v1/investor/delete/<str:first_name>")
 
 class BusinessUrlTestsCase(TestCase):
     """
